726c40d5742cc6bcc1d8c3d8a390c40ba87c99d3
Edmund-Frio <edmundfrio@gmail.com>
2023-07-26 12:34:02 -1000

    added # number of results functionality

--- main.js
+++ main.js
@@ -14,17 +14,21 @@ function displayResults(searchQuery) {
     .then((response) => response.json())
     .then((data) => {
       const resultsArray = data.query.search;
-      results(resultsArray);
+      const resultsInfo = data.query.searchinfo;
+      results(resultsArray, resultsInfo);
+      console.log(resultsArray)
     })
     .catch(() => {
       console.log("Error! Could not search Wikipedia API!");
     });
 
-  function results(array) {
+  function results(array, info) {
+    const totalResults = info.totalhits
+
     resultsContent.innerHTML = "";
     resultsContent.insertAdjacentHTML(
       "beforeend",
-      `<h1>Seeing results for ${searchInput.value}...</h1>`
+      `<h1 id="total-results">Seeing ${totalResults} results for ${searchInput.value}...</h1>`
     );
     array.forEach((article) => {
       const articleTitle = article.title;
@@ -35,11 +39,11 @@ function displayResults(searchQuery) {
 
       resultsContent.insertAdjacentHTML(
         "beforeend",
-        `<div class="resultArticle">
-    <h2 class="resultTitle">
+        `<div class="result-article">
+    <h2 class="result-title">
     <a href="${articleUrl}" target="_blank" rel="noopener">${articleTitle}</a>
     </h2>
-    <p class="resultSnippet"><a href="${articleUrl}" target="_blank" rel="noopener">
+    <p class="result-snippet"><a href="${articleUrl}" target="_blank" rel="noopener">
   ${articleSnippet}</a></p>
     </div>`
       );
